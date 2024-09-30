0be6fbb02af2951fc66d1325a533677f31eeebc5
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-09-30 09:39:11 +0300

    .

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1d001811e3fb03950071f67b8752302a72aa33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec120b79d0c1c1bbea21a8aebae8286917be15b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
